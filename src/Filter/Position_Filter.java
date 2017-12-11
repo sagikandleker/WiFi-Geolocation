8502a654a4e8561f07ae5ec37324517610cd79a2
iby1812 <33149774+iby1812@users.noreply.github.com>
2017-12-11 14:47:51 +0200

    fixed radius problem.

--- src/Filter/Position_Filter.java
+++ src/Filter/Position_Filter.java
@@ -74,6 +74,7 @@ public class Position_Filter {
 	private static double deg2rad(double deg) {
 		return (deg * Math.PI / 180.0);
 	}
+	
 	/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
 	/*::	This function converts radians to decimal degrees						 :*/
 	/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
