c137beba0103c5fd57ae2e44e78982fb6aa4526d
iby1812 <33149774+iby1812@users.noreply.github.com>
2017-12-14 00:44:57 +0200

    Added user option to choose.

--- src/Main_App/Main.java
+++ src/Main_App/Main.java
@@ -2,6 +2,7 @@ package Main_App;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.Scanner;
 
 import Algo_Search.Algorithm1;
 
@@ -15,16 +16,22 @@ import Algo_Search.Algorithm1;
  */
 
 public class Main {
-	public static final String path = "C:/Users/sagik/git/OOP";
+	public static final String path = "C:\\Users\\itay1\\git\\OOP";
 	public static final String path_In = path+"/Files read from";
 	public static final String path_Out= path+"/Files create to";
 	public static final String file_Out = path_Out+"/WigleWifi_Sorted.csv";
 
 	public static void main(String[] args) throws IOException, ParseException {
-		
 		Files_2_CSV.getFiles();
-		Algorithm1.Better("a4:2b:b0:ad:2d:34");
-		//Algorithm1.Naive("a4:2b:b0:ad:2d:34");
+		Scanner sc = new Scanner(System.in);
+		String input_choice;
+		System.out.println("What do you want to do next? KML / Algo ?");
+		input_choice=sc.nextLine();
+		if(input_choice.equalsIgnoreCase("KML")) Filter.Main_Filter.Choosefilter();
+		else if(input_choice.equalsIgnoreCase("Algo")) Algo_Search.Algo_Main.ChooseAlgo();
+			
+		
+		
 		System.out.println("Finish All");
 		
 				
