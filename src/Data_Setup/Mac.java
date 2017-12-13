13d3fb43ae3f9a5fbf495d354630cb8d4840eab1
Sagi Kandleker <33149808+sagikandleker@users.noreply.github.com>
2017-12-13 17:33:55 +0200

    New Mac class

---
+++ src/Data_Setup/Mac.java
@@ -0,0 +1,24 @@
+package Data_Setup;
+
+public class Mac {
+	String mac;
+
+	public Mac(String mac) {
+		this.mac = mac;
+	}
+
+	public String getMac() {
+		return mac;
+	}
+
+	public void setMac(String mac) {
+		this.mac = mac;
+	}
+
+	@Override
+	public String toString() {
+		return mac;
+	}
+	
+	
+}
