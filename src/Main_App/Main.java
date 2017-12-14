4a3ee8e4cc4486cfc447f73c84ff6ffc730275ed
Sagi Kandleker <33149808+sagikandleker@users.noreply.github.com>
2017-12-14 10:23:10 +0200

    Path Changed

--- src/Main_App/Main.java
+++ src/Main_App/Main.java
@@ -16,20 +16,21 @@ import Algo_Search.Algorithm1;
  */
 
 public class Main {
-	public static final String path = "C:\\Users\\itay1\\git\\OOP";
+	public static final String path = "C:\\Users\\sagik\\git\\OOP";
 	public static final String path_In = path+"/Files read from";
 	public static final String path_Out= path+"/Files create to";
 	public static final String file_Out = path_Out+"/WigleWifi_Sorted.csv";
 
 	public static void main(String[] args) throws IOException, ParseException {
 		Files_2_CSV.getFiles();
+		
 		Scanner sc = new Scanner(System.in);
 		String input_choice;
 		System.out.println("What do you want to do next? KML / Algo ?");
 		input_choice=sc.nextLine();
 		if(input_choice.equalsIgnoreCase("KML")) Filter.Main_Filter.Choosefilter();
 		else if(input_choice.equalsIgnoreCase("Algo")) Algo_Search.Algo_Main.ChooseAlgo();
-			
+		sc.close();	
 		
 		
 		System.out.println("Finish All");
