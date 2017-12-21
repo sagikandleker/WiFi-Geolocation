5bd633403c73bab0778e0b394e8814c5b21b9dda
Sagi Kandleker <33149808+sagikandleker@users.noreply.github.com>
2017-12-21 20:36:57 +0200

    Added Record Mac and Signal

---
+++ src/Data_Setup/Record_Mac_Signal.java
@@ -0,0 +1,38 @@
+package Data_Setup;
+
+public class Record_Mac_Signal {
+
+	public Signal signal;
+	public Mac mac;
+	
+	public Record_Mac_Signal() {
+		this.signal = new Signal();
+		this.mac = new Mac("");
+	}
+
+	public Record_Mac_Signal(Mac mac, Signal signal) {
+		this.signal = signal;
+		this.mac = mac;
+	}
+	public Record_Mac_Signal(String mac, int signal) {
+		this.signal.signal = signal;
+		this.mac.mac = mac;
+	}
+	
+	public Signal getSignal() {
+		return signal;
+	}
+
+	public void setSignal(Signal signal) {
+		this.signal = signal;
+	}
+
+	public Mac getMac() {
+		return mac;
+	}
+
+	public void setMac(Mac mac) {
+		this.mac = mac;
+	}
+	
+}
