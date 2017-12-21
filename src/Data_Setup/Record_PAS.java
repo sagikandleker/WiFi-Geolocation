e3b1b4e74f2642ddeb840346a7ed7ef30bbb9c4f
Sagi Kandleker <33149808+sagikandleker@users.noreply.github.com>
2017-12-21 20:37:52 +0200

    Update

--- src/Data_Setup/Record_PAS.java
+++ src/Data_Setup/Record_PAS.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public class Record_PAS {
 	public Position position;
 	public Signal signal;
-	
+
 	public Record_PAS() {
 		this.position = new Position(0,0,0);
 		this.signal = new Signal(0);
 	}
-	
+
 	public Record_PAS(Position position, Signal signal) {
 		this.position = position;
 		this.signal = signal;
@@ -31,38 +31,46 @@ public class Record_PAS {
 	public void setSignal(Signal signal) {
 		this.signal = signal;
 	}
+	
 
 	@Override
 	public String toString() {
 		return position + "," + signal;
 	}
-	
-	
+
+
 	/**
 	 *  sort_Signal_Power sorting the list from the stronger signal to the lower.
 	 */
 	public static ArrayList<Record_PAS> sort_Signal_Power(ArrayList<Record_PAS> pas)
 	{
 		
-		for (int i = 0; i < pas.size(); i++)
-		{
-			for (int j = 0; j < pas.size(); j++)
+			for (int i = 0; i < pas.size(); i++)
 			{
-				if((pas.get(i).getSignal().bigger(pas.get(j).getSignal())))
+				for (int j = 0; j < pas.size(); j++)
 				{
-					Record_PAS temp;
-					temp=pas.get(i);
-					pas.set(i, pas.get(j));
-					pas.set(j, temp);
+					if((pas.get(i).getSignal().bigger(pas.get(j).getSignal())))
+					{
+						Record_PAS temp;
+						temp=pas.get(i);
+						pas.set(i, pas.get(j));
+						pas.set(j, temp);
+					}
 				}
 			}
+			
+			if(pas.size() >= 3) {
+			ArrayList<Record_PAS> three_pos = new ArrayList<Record_PAS>();
+			for (int k = 0; k < 3; k++)
+			{
+				three_pos.add(pas.get(k));
+			}
+			return three_pos;
 		}
-		
-		ArrayList<Record_PAS> three_pos = new ArrayList<Record_PAS>();
-		for (int k = 0; k < 3; k++)
-		{
-			three_pos.add(pas.get(k));
+
+		else {
+			return pas;
 		}
-		return three_pos;
 	}
+
 }
