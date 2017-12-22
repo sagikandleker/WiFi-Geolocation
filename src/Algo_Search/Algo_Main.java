86fe7fdeabfabf4d90567e6094248f22d1db8234
guy <guy@DESKTOP-CBSASJ8>
2017-12-22 10:53:00 +0200

    TODO line

--- src/Algo_Search/Algo_Main.java
+++ src/Algo_Search/Algo_Main.java
@@ -9,11 +9,11 @@ import Data_Setup.Record_Mac_Signal;
 public class Algo_Main {
 
 	public static void ChooseAlgo() throws IOException {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		String choice;
 		System.out.println("Select an Algorithm you'd like to use: algo1 / algo2 ?");
 		choice=sc.nextLine();
+		//TODO Change String MAC to Mac mac(Object)
 		if(choice.equalsIgnoreCase("algo1"))
 		{
 			String mac;
