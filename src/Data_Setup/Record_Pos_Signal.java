5973b1fe38ce52d152b307a62a17c8c2517ebe50
Sagi Kandleker <33149808+sagikandleker@users.noreply.github.com>
2017-12-22 10:34:49 +0200

    Changed Algo1 to Hash

--- src/Data_Setup/Record_Pos_Signal.java
+++ src/Data_Setup/Record_Pos_Signal.java
@@ -1,14 +1,21 @@
 package Data_Setup;
 
+import java.util.ArrayList;
+
 public class Record_Pos_Signal {
 
-	Position position;
-	Signal signal;
+	public Position position;
+	public Signal signal;
 	
 	public Record_Pos_Signal() {
 		this.position = new Position();
 		this.signal = new Signal();
 	}
+	
+	/*public Record_Pos_Signal() {
+		this.position = new Position(0,0,0);
+		this.signal = new Signal(0);
+	}*/
 	
 	public Record_Pos_Signal(Position position, Signal signal) {
 		this.position = position;
@@ -41,6 +48,39 @@ public class Record_Pos_Signal {
 		return position + "," + signal;
 	}
 	
-	
+	/**
+	 *  sort_Signal_Power sorting the list from the stronger signal to the lower.
+	 */
+	public static ArrayList<Record_Pos_Signal> sort_Signal_Power(ArrayList<Record_Pos_Signal> pas)
+	{
+		
+			for (int i = 0; i < pas.size(); i++)
+			{
+				for (int j = 0; j < pas.size(); j++)
+				{
+					if((pas.get(i).getSignal().bigger(pas.get(j).getSignal())))
+					{
+						Record_Pos_Signal temp;
+						temp=pas.get(i);
+						pas.set(i, pas.get(j));
+						pas.set(j, temp);
+					}
+				}
+			}
+			
+			if(pas.size() >= 3) {
+			ArrayList<Record_Pos_Signal> three_pos = new ArrayList<Record_Pos_Signal>();
+			
+			for (int k = 0; k < 3; k++)
+			{
+				three_pos.add(pas.get(k));
+			}
+			return three_pos;
+		}
+
+		else {
+			return pas;
+		}
+	}
 	
 }
