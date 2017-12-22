cb0cff94a0577cae28a285ec2ef0e535b0e128de
Sagi Kandleker <33149808+sagikandleker@users.noreply.github.com>
2017-12-22 10:34:26 +0200

    Clone class

--- src/Data_Setup/Record_PAS.java
+++ src/Data_Setup/Record_PAS.java
@@ -6,10 +6,7 @@ public class Record_PAS {
 	public Position position;
 	public Signal signal;
 
-	public Record_PAS() {
-		this.position = new Position(0,0,0);
-		this.signal = new Signal(0);
-	}
+	
 
 	public Record_PAS(Position position, Signal signal) {
 		this.position = position;
@@ -39,38 +36,6 @@ public class Record_PAS {
 	}
 
 
-	/**
-	 *  sort_Signal_Power sorting the list from the stronger signal to the lower.
-	 */
-	public static ArrayList<Record_PAS> sort_Signal_Power(ArrayList<Record_PAS> pas)
-	{
-		
-			for (int i = 0; i < pas.size(); i++)
-			{
-				for (int j = 0; j < pas.size(); j++)
-				{
-					if((pas.get(i).getSignal().bigger(pas.get(j).getSignal())))
-					{
-						Record_PAS temp;
-						temp=pas.get(i);
-						pas.set(i, pas.get(j));
-						pas.set(j, temp);
-					}
-				}
-			}
-			
-			if(pas.size() >= 3) {
-			ArrayList<Record_PAS> three_pos = new ArrayList<Record_PAS>();
-			for (int k = 0; k < 3; k++)
-			{
-				three_pos.add(pas.get(k));
-			}
-			return three_pos;
-		}
-
-		else {
-			return pas;
-		}
-	}
+	
 
 }
