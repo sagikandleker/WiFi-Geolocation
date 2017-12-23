fe05bea6963a8ac44abfb2ffacf7ae4a27d0c3cc
Sagi Kandleker <33149808+sagikandleker@users.noreply.github.com>
2017-12-23 20:15:47 +0200

    Record to Algorithm function

---
+++ src/Data_Setup/Record_Pos_Wifi_Time.java
@@ -0,0 +1,90 @@
+package Data_Setup;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Record_Pos_Wifi_Time {
+
+	private Position position;
+	private Wifi wifi;
+	private Time time;
+	
+	
+	public Record_Pos_Wifi_Time() {
+		this.position = new Position();
+		this.wifi = new Wifi();
+		this.time = new Time();
+	}
+	
+	public Record_Pos_Wifi_Time(Position position, Wifi wifi, Time time) {
+		this.position = position;
+		this.wifi = wifi;
+		this.time = time;
+	}
+	
+	
+	public Position getPosition() {
+		return position;
+	}
+
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+
+	public Wifi getWifi() {
+		return wifi;
+	}
+
+	public void setWifi(Wifi wifi) {
+		this.wifi = wifi;
+	}
+	
+	public Time getTime() {
+		return time;
+	}
+
+	public void setTime(Date time) {
+		this.time = new Time(time);
+	}
+
+	@Override
+	public String toString() {
+		return wifi + "," + position + "," + time;
+	}
+	
+	/**
+	 *  sort_Signal_Power sorting the list from the stronger signal to the lower.
+	 */
+	public static ArrayList<Record_Pos_Wifi_Time> sort_Signal_Power(ArrayList<Record_Pos_Wifi_Time> pas)
+	{
+		
+			for (int i = 0; i < pas.size(); i++)
+			{
+				for (int j = 0; j < pas.size(); j++)
+				{
+					if((pas.get(i).getWifi().getSignal()>pas.get(j).getWifi().getSignal()))
+					{
+						Record_Pos_Wifi_Time temp;
+						temp=pas.get(i);
+						pas.set(i, pas.get(j));
+						pas.set(j, temp);
+					}
+				}
+			}
+			
+			if(pas.size() >= 3) {
+			ArrayList<Record_Pos_Wifi_Time> three_pos = new ArrayList<Record_Pos_Wifi_Time>();
+			
+			for (int k = 0; k < 3; k++)
+			{
+				three_pos.add(pas.get(k));
+			}
+			return three_pos;
+		}
+
+		else {
+			return pas;
+		}
+	}
+	
+}
