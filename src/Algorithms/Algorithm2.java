b2d7c9908d478a639707d2c4b1d3180115306430
Sagi Kandleker <33149808+sagikandleker@users.noreply.github.com>
2017-12-23 20:19:52 +0200

    Added functions to Algo1 and Write to CSV

---
+++ src/Algorithms/Algorithm2.java
@@ -0,0 +1,18 @@
+package Algorithms;
+
+import java.util.ArrayList;
+
+import Data_Setup.Record_Mac_Signal;
+
+public class Algorithm2 {
+
+	public static void Algo2(ArrayList<Record_Mac_Signal> mac_signal)
+	{
+		
+		
+		
+		
+	}
+	
+	
+}
