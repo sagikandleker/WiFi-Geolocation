d77e582fb43e52ffc35b5580b07bf0fb16c2b374
Sagi Kandleker <33149808+sagikandleker@users.noreply.github.com>
2017-12-23 20:20:07 +0200

    Changed the ID problem

---
+++ src/Filtering/Filter_2_KML.java
@@ -0,0 +1,31 @@
+package Filtering;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import de.micromata.opengis.kml.v_2_2_0.Document;
+import de.micromata.opengis.kml.v_2_2_0.Kml;
+import de.micromata.opengis.kml.v_2_2_0.Placemark;
+import Main_App.Main;
+
+public class Filter_2_KML {
+
+	public static void KML(ArrayList<Record_Filter> data,String type) throws IOException	
+	{
+		final Kml kml = new Kml();
+		Document doc = kml.createAndSetDocument();
+
+		for (int i = 0; i < data.size(); i++) 
+		{
+			String date = data.get(i).date.toString();
+			Placemark place =  doc.createAndAddPlacemark().withName(data.get(i).getId()).withOpen(Boolean.TRUE);
+			place.createAndSetPoint().addToCoordinates(data.get(i).position.lon,data.get(i).position.lat,data.get(i).position.alt);
+			date = date.replace(' ', 'T')+'Z';
+			place.createAndSetTimeStamp().setWhen(date);
+		}
+		kml.marshal(new File(Main.path_Out+"\\Filter_"+type+".kml"));
+		System.out.println("Finish to create KML File");
+	}
+
+
+}
