1fbe9a74ee674caff9952ae2bd3a9a8974866679
iby1812 <33149774+iby1812@users.noreply.github.com>
2017-12-25 14:14:29 +0200

    Update

--- src/Algorithms/Algorithm1.java
+++ src/Algorithms/Algorithm1.java
@@ -33,7 +33,7 @@ public class Algorithm1 {
 		int size=0;
 		while(size<spwt.size())
 		{
-			all_parameters.setWeight(((1/Math.pow((spwt.get(0).getWifi().getSignal()),2.0))));
+			all_parameters.setWeight(((1/Math.pow((spwt.get(size).getWifi().getSignal()),2.0))));
 			all_parameters.setWeightsum(all_parameters.getWeight());
 			all_parameters.setWlatsum(spwt.get(size).getPosition().getLat()*(all_parameters.getWeight()));
 			all_parameters.setWlonsum(spwt.get(size).getPosition().getLon()*all_parameters.getWeight());
@@ -42,7 +42,7 @@ public class Algorithm1 {
 			pos.getPosition().setLat((all_parameters.getWlatsum())/(all_parameters.getWeightsum()));
 			pos.getPosition().setLon((all_parameters.getWlonsum())/(all_parameters.getWeightsum()));
 			pos.getPosition().setAlt((all_parameters.getWaltsum())/(all_parameters.getWeightsum()));
-			pos.getWifi().setSignal((spwt.get(size).getWifi().getSignal()));
+			pos.getWifi().setSignal((spwt.get(0).getWifi().getSignal()));
 			pos.getWifi().setMac(spwt.get(size).getWifi().getMac());
 			pos.getWifi().setFrequency(spwt.get(size).getWifi().getFrequency());
 			pos.getWifi().setSsid(spwt.get(size).getWifi().getSsid());
@@ -50,121 +50,6 @@ public class Algorithm1 {
 			
 			size++;
 		}
-		
-		
-		/*
-		
-		if(three_pos.size() == 1) {
-		
-			pos.getPosition().setLat(three_pos.get(0).getPosition().getLat());
-			pos.getPosition().setLon(three_pos.get(0).getPosition().getLon());
-			pos.getPosition().setAlt(three_pos.get(0).getPosition().getAlt());
-			pos.getWifi().setSignal((three_pos.get(0).getWifi().getSignal()));
-			pos.getWifi().setMac(three_pos.get(0).getWifi().getMac());
-			pos.getWifi().setFrequency(three_pos.get(0).getWifi().getFrequency());
-			pos.getWifi().setSsid(three_pos.get(0).getWifi().getSsid());
-			pos.getTime().setTime(three_pos.get(0).getTime());
-			
-		}
-			
-		else if(three_pos.size() == 2) {
-
-			all_parameters.setWeight1(((1/Math.pow((three_pos.get(0).getWifi().getSignal()),2.0))));
-			all_parameters.setWlat1(three_pos.get(0).getPosition().getLat()*(all_parameters.getWeight1()));
-			all_parameters.setWlon1(three_pos.get(0).getPosition().getLon()*all_parameters.getWeight1());
-			all_parameters.setWalt1(three_pos.get(0).getPosition().getAlt()*all_parameters.getWeight1());
-
-			all_parameters.setWeight2(1/Math.pow((three_pos.get(1).getWifi().getSignal()),2.0));
-			all_parameters.setWlat2(three_pos.get(1).getPosition().getLat()*all_parameters.getWeight2());
-			all_parameters.setWlon2(three_pos.get(1).getPosition().getLon()*all_parameters.getWeight2());
-			all_parameters.setWalt2(three_pos.get(1).getPosition().getAlt()*all_parameters.getWeight2());
-
-			all_parameters.setWeightsum();
-			all_parameters.setWlatsum();
-			all_parameters.setWlonsum();
-			all_parameters.setWaltsum();
-
-			pos.getPosition().setLat((all_parameters.getWlatsum())/(all_parameters.getWeightsum()));
-			pos.getPosition().setLon((all_parameters.getWlonsum())/(all_parameters.getWeightsum()));
-			pos.getPosition().setAlt((all_parameters.getWaltsum())/(all_parameters.getWeightsum()));
-			pos.getWifi().setSignal((three_pos.get(0).getWifi().getSignal()));
-			pos.getWifi().setMac(three_pos.get(0).getWifi().getMac());
-			pos.getWifi().setFrequency(three_pos.get(0).getWifi().getFrequency());
-			pos.getWifi().setSsid(three_pos.get(0).getWifi().getSsid());
-			pos.getTime().setTime(three_pos.get(0).getTime());
-		}
-		else if(three_pos.size() == 3) {
-
-			all_parameters.setWeight1(((1/Math.pow((three_pos.get(0).getWifi().getSignal()),2.0))));
-			all_parameters.setWlat1((three_pos.get(0).getPosition().getLat())*(all_parameters.getWeight1()));
-			all_parameters.setWlon1((three_pos.get(0).getPosition().getLon())*(all_parameters.getWeight1()));
-			all_parameters.setWalt1((three_pos.get(0).getPosition().getAlt())*(all_parameters.getWeight1()));
-
-			all_parameters.setWeight2(1/Math.pow((three_pos.get(1).getWifi().getSignal()),2.0));
-			all_parameters.setWlat2((three_pos.get(1).getPosition().getLat())*(all_parameters.getWeight2()));
-			all_parameters.setWlon2((three_pos.get(1).getPosition().getLon())*(all_parameters.getWeight2()));
-			all_parameters.setWalt2((three_pos.get(1).getPosition().getAlt())*(all_parameters.getWeight2()));
-
-			all_parameters.setWeight3(1/Math.pow((three_pos.get(2).getWifi().getSignal()),2.0));
-			all_parameters.setWlat3((three_pos.get(2).getPosition().getLat())*(all_parameters.getWeight3()));
-			all_parameters.setWlon3((three_pos.get(2).getPosition().getLon())*(all_parameters.getWeight3()));
-			all_parameters.setWalt3((three_pos.get(2).getPosition().getAlt())*(all_parameters.getWeight3()));
-
-			all_parameters.setWeightsum();
-			all_parameters.setWlatsum();
-			all_parameters.setWlonsum();
-			all_parameters.setWaltsum();
-
-			pos.getPosition().setLat((all_parameters.getWlatsum())/(all_parameters.getWeightsum()));
-			pos.getPosition().setLon((all_parameters.getWlonsum())/(all_parameters.getWeightsum()));
-			pos.getPosition().setAlt((all_parameters.getWaltsum())/(all_parameters.getWeightsum()));
-			pos.getWifi().setSignal((three_pos.get(0).getWifi().getSignal()));
-			pos.getWifi().setMac(three_pos.get(0).getWifi().getMac());
-			pos.getWifi().setFrequency(three_pos.get(0).getWifi().getFrequency());
-			pos.getWifi().setSsid(three_pos.get(0).getWifi().getSsid());
-			pos.getTime().setTime(three_pos.get(0).getTime());
-				
-		}
-		
-		else if(three_pos.size() == 4) {
-
-			all_parameters.setWeight1(((1/Math.pow((three_pos.get(0).getWifi().getSignal()),2.0))));
-			all_parameters.setWlat1((three_pos.get(0).getPosition().getLat())*(all_parameters.getWeight1()));
-			all_parameters.setWlon1((three_pos.get(0).getPosition().getLon())*(all_parameters.getWeight1()));
-			all_parameters.setWalt1((three_pos.get(0).getPosition().getAlt())*(all_parameters.getWeight1()));
-
-			all_parameters.setWeight2(1/Math.pow((three_pos.get(1).getWifi().getSignal()),2.0));
-			all_parameters.setWlat2((three_pos.get(1).getPosition().getLat())*(all_parameters.getWeight2()));
-			all_parameters.setWlon2((three_pos.get(1).getPosition().getLon())*(all_parameters.getWeight2()));
-			all_parameters.setWalt2((three_pos.get(1).getPosition().getAlt())*(all_parameters.getWeight2()));
-
-			all_parameters.setWeight3(1/Math.pow((three_pos.get(2).getWifi().getSignal()),2.0));
-			all_parameters.setWlat3((three_pos.get(2).getPosition().getLat())*(all_parameters.getWeight3()));
-			all_parameters.setWlon3((three_pos.get(2).getPosition().getLon())*(all_parameters.getWeight3()));
-			all_parameters.setWalt3((three_pos.get(2).getPosition().getAlt())*(all_parameters.getWeight3()));
-
-			
-			all_parameters.setWeight4(1/Math.pow((three_pos.get(2).getWifi().getSignal()),2.0));
-			all_parameters.setWlat4((three_pos.get(2).getPosition().getLat())*(all_parameters.getWeight3()));
-			all_parameters.setWlon4((three_pos.get(2).getPosition().getLon())*(all_parameters.getWeight3()));
-			all_parameters.setWalt4((three_pos.get(2).getPosition().getAlt())*(all_parameters.getWeight3()));
-			
-			all_parameters.setWeightsum();
-			all_parameters.setWlatsum();
-			all_parameters.setWlonsum();
-			all_parameters.setWaltsum();
-
-			pos.getPosition().setLat((all_parameters.getWlatsum())/(all_parameters.getWeightsum()));
-			pos.getPosition().setLon((all_parameters.getWlonsum())/(all_parameters.getWeightsum()));
-			pos.getPosition().setAlt((all_parameters.getWaltsum())/(all_parameters.getWeightsum()));
-			pos.getWifi().setSignal((three_pos.get(0).getWifi().getSignal()));
-			pos.getWifi().setMac(three_pos.get(0).getWifi().getMac());
-			pos.getWifi().setFrequency(three_pos.get(0).getWifi().getFrequency());
-			pos.getWifi().setSsid(three_pos.get(0).getWifi().getSsid());
-			pos.getTime().setTime(three_pos.get(0).getTime());
-				
-		}
-		*/
 		rpwt.add(pos);
 	}
 
