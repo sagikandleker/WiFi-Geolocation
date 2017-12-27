ec4d96f2bb3f42dcf5417a29de3799bf27319ff6
Sagi Kandleker <33149808+sagikandleker@users.noreply.github.com>
2017-12-28 00:01:37 +0200

    Update

--- src/Algorithms/Mac_Hashmap.java
+++ src/Algorithms/Mac_Hashmap.java
@@ -18,11 +18,11 @@ public class Mac_Hashmap {
 
 	public static HashMap<String,ArrayList<Record_Pos_Wifi_Time>> hash = new HashMap<String,ArrayList<Record_Pos_Wifi_Time>>();
 	static ArrayList<Record> All_Data_List = Write_2_CSV.All_Data_List;
+	//static ArrayList<Record> data = Read_From.data;
 
-	public static void Build_Hash() {
-
+	public static void Build_Hash(ArrayList<Record> data, String nameOfHash) {
+		
 		Record_Pos_Wifi_Time pos_wifi_time;
-
 		Mac mac;
 		Signal signal;
 		SSID ssid;
@@ -31,14 +31,14 @@ public class Mac_Hashmap {
 		Position position;
 		Wifi wifi;
 
-		for (int i = 0; i < All_Data_List.size(); i++) {
-			position = All_Data_List.get(i).getPosition();
-			for (int k = 0; k < All_Data_List.get(i).getWifiList().size(); k++) {
-				mac = new Mac(All_Data_List.get(i).getWifiList().get(k).getMac());
-				signal = new Signal((All_Data_List.get(i).getWifiList().get(k).getSignal()));
-				ssid = new SSID(All_Data_List.get(i).getWifiList().get(k).getSsid());
-				time = new Time(All_Data_List.get(i).getDate());
-				frequency =  new Frequency(All_Data_List.get(i).getWifiList().get(k).getFrequency());
+		for (int i = 0; i < data.size(); i++) {
+			position = data.get(i).getPosition();
+			for (int k = 0; k < data.get(i).getWifiList().size(); k++) {
+				mac = new Mac(data.get(i).getWifiList().get(k).getMac());
+				signal = new Signal((data.get(i).getWifiList().get(k).getSignal()));
+				ssid = new SSID(data.get(i).getWifiList().get(k).getSsid());
+				time = new Time(data.get(i).getDate());
+				frequency =  new Frequency(data.get(i).getWifiList().get(k).getFrequency());
 				wifi = new Wifi(ssid, mac, frequency, signal);
 				pos_wifi_time = new Record_Pos_Wifi_Time(position, wifi, time);
 
