5fd72177098f2cf29efd5521c30220462e0eff9e
Sagi Kandleker <33149808+sagikandleker@users.noreply.github.com>
2017-12-28 13:59:05 +0200

    2 new functions

--- src/Main_App/Read_From.java
+++ src/Main_App/Read_From.java
@@ -1,19 +1,19 @@
 package Main_App;
 
 import java.io.BufferedReader;
-
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
-import Algorithms.Mac_Hashmap;
 import Data_Setup.Frequency;
 import Data_Setup.Mac;
 import Data_Setup.Position;
 import Data_Setup.Record;
+import Data_Setup.Record_Mac_Signal;
 import Data_Setup.SSID;
 import Data_Setup.Signal;
 import Data_Setup.Time;
@@ -22,7 +22,7 @@ import Data_Setup.Wifi;
 public class Read_From {
 	//public static final ArrayList<Record> data = new ArrayList<Record>();
 
-	public static ArrayList<Record> CSV_File(String file) throws IOException, ParseException {
+	public static ArrayList<Record> comb_File(String file) throws IOException, ParseException {
 
 		Mac mac;
 		Signal signal;
@@ -39,9 +39,9 @@ public class Read_From {
 
 		while(line!=null) {
 			String[] arr = (line.split(","));
-			
+			System.out.println(Arrays.toString(arr));
 			if(arr[1].startsWith("model")) {
-				
+
 				Date date  = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(arr[0]);
 				Time time = new Time(date);
 				position = new Position(Double.parseDouble(arr[2]),Double.parseDouble(arr[3]),Double.parseDouble(arr[4]));
@@ -61,10 +61,42 @@ public class Read_From {
 					line = br.readLine();
 
 				}	
-				
+
 			}
 		}
-		//System.out.println(data);
+		System.out.println(data);
+		br.close();
+		return data;
+	}
+
+	public static ArrayList<ArrayList<Record_Mac_Signal>> nogps_File(String file) throws IOException {
+
+		Mac mac;
+		Signal signal;
+		Record_Mac_Signal rms;
+		ArrayList<ArrayList<Record_Mac_Signal>> data = new ArrayList<ArrayList<Record_Mac_Signal>>();
+		
+		FileReader fr = new FileReader(file);
+		BufferedReader br = new BufferedReader(fr);
+		String line = br.readLine();
+
+		while(line!=null) {
+
+			String[] arr = (line.split(","));
+			ArrayList<Record_Mac_Signal> line_data = new ArrayList<Record_Mac_Signal>();
+			for (int m = 7; m < arr.length; m+=4) {
+
+					mac = new Mac(arr[m]);
+					signal = new Signal(arr[m+2]);
+					rms = new Record_Mac_Signal(mac, signal);
+					line_data.add(rms);
+				
+			}
+			
+			data.add(line_data);
+			line = br.readLine();
+			
+		}
 		br.close();
 		return data;
 
