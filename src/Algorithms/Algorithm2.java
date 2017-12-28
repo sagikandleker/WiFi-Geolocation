1441c2b6d4caf2d9fcf7c61ea4a29d4e3d4226e9
Sagi Kandleker <33149808+sagikandleker@users.noreply.github.com>
2017-12-28 13:58:21 +0200

    Update

--- src/Algorithms/Algorithm2.java
+++ src/Algorithms/Algorithm2.java
@@ -1,24 +1,25 @@
 package Algorithms;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-import Data_Setup.Record;
+import Data_Setup.Record_Mac_Signal;
+import Data_Setup.Record_Pos_Wifi_Time;
 
 public class Algorithm2 {
 	
-	public static void Algo2(Object build_Hash) {
+	public static void Algo2(HashMap<String,ArrayList<Record_Pos_Wifi_Time>> algo2_Hash) {
 		
 		System.out.println("This the HashMap with gps");
-		System.out.println(Mac_Hashmap.hash);
+		System.out.println(algo2_Hash);
 		
 	}
 	
-	public static void Algo2_nogps(ArrayList<Record> data) {
+	public static void Algo2_nogps(ArrayList<ArrayList<Record_Mac_Signal>> data) {
 		
 		System.out.println("This the nogps ArrayList");
 		System.out.println(data);
 		
-		
 	}
-	
+
 }
