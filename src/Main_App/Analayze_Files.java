044e5cbcdeb1305b51accf50e396763816c4f34b
Sagi Kandleker <sagikandleker@gmail.com>
2018-01-02 14:15:15 +0200

    change parameters that send to function

--- src/Main_App/Analayze_Files.java
+++ src/Main_App/Analayze_Files.java
@@ -11,25 +11,17 @@ public class Analayze_Files {
 	 *  getFiles Call to Write_Headers function and check all the CSV files in the folder,
 	 * and sending them to Analyze function.
 	 */
-	public static void getFiles() throws IOException, ParseException {
-		Write_2_CSV.Write_Headers();
-		File folder = new File(Main.path_In_WigleWifi);
+	public static void getFiles(File folder) throws IOException, ParseException {
+		
 		String[] files_Names = folder.list();
 
-		int counter = 0;
 		for(int i = 0; i < files_Names.length; i++) {
 			if(files_Names[i].startsWith("WigleWifi") && files_Names[i].endsWith(".csv")) {
-				Read_From.wigle_File(Main.path_In_WigleWifi+"/"+files_Names[i]);
-				counter++;
+				File file = new File(folder+"/"+files_Names[i]);
+				Read_From.wigle_File(file);
+
 			}
 		}
 		
-		Write_2_CSV.Write();
-		System.out.println(counter + " CSV files founded ["+Main.path_In+"]");
-		
-		if(counter != 0) {
-			System.out.println("1 CSV has been created ["+Main.path_Out_WigleWifi+"]\n");
-
-		}
 	}
 }
